979cb36c68150bc5d13ec63eb7f6111e42996571
Saimon Moore <saimon.moore@xing.com>
2019-12-01 14:37:00 +0100

    Simple json/graphql-backed Groups component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,24 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `PIBON ALS 40`,
+    description: `Celebrem els 40 anys maravilloses de l'Eulalia Pinyol Olle`,
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-transformer-json`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data`,
       },
     },
     `gatsby-transformer-sharp`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,52 @@
 import React from "react"
-import { Link } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import ContentQuery from "../queries/content.gql"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+const Groups = ({
+  data: {
+    allContentJson: { edges },
+  },
+}) => (
+  <ul>
+    {edges.map(({ node }, i) => (
+      <li>{node.name}</li>
+    ))}
+  </ul>
 )
+
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query ContentQuery {
+      allContentJson {
+        edges {
+          node {
+            name
+            description
+            content {
+              name
+              photo
+              video
+              words
+              password
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Groups data={data} />
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
