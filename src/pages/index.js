cc2863bd486dbfb968fd2ecd1b184b150a08f744
Saimon Moore <saimon.moore@xing.com>
2019-12-01 14:54:14 +0100

    Remove gql files

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import { useStaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import ContentQuery from "../queries/content.gql"
 
 const Groups = ({
   data: {
